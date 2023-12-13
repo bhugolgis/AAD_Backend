733c4dca69ae09f1f2aed8556858934f9fbdaed9
Jafar Khan <jafar.khan@AMIAND.LOCAL>
2023-12-13 15:55:19 +0530

    fix get CHV and ASHA list API

---
+++ adminportal/signals.py
@@ -0,0 +1,69 @@
+from django.db.models.signals import post_save , pre_save
+from django.dispatch import receiver
+from database.models import familyHeadDetails , CustomUser , familyMembers , UserApprovalRecords
+from django.contrib.auth.models import Group
+from django.contrib.auth.hashers import make_password
+
+@receiver(post_save, sender= UserApprovalRecords)
+def update_user_group(sender, instance,created , **kwargs):
+    """
+    The function creates a new user for a family head and assigns them to the "Family Head" group.
+    
+    :param sender: The `sender` parameter refers to the object that is sending the signal. In this case,
+    it could be any model instance that triggers the signal
+    :param instance: The `instance` parameter is an instance of the model that triggered the signal. In
+    this case, it is likely an instance of a model representing a family head
+    """
+
+    if created: 
+        try:
+            instance = Group.objects.get(name=instance.group__name)
+            print(instance)
+            return instance
+        except Exception:
+            return None
+        
+        
+        # new_instance = CustomUser.objects.create_user(phoneNumber=instance.mobileNo , username= instance.mobileNo , password=password )
+        # if new_instance:
+        #     group = Group.objects.get(name = 'Family Head')
+        #     new_instance.groups.add(group)
+          
+
+
+# @receiver(post_save, sender=familyMembers)
+# def Update_Partial_Submit_Field(sender, instance, created , **kwargs):
+#     """
+#     The function creates a new user for a family head and assigns them to the "Family Head" group.
+    
+#     :param sender: The `sender` parameter refers to the object that is sending the signal. In this case,
+#     it could be any model instance that triggers the signal
+#     :param instance: The `instance` parameter is an instance of the model that triggered the signal. In
+#     this case, it is likely an instance of a model representing a family head
+#     """
+
+#     if created: 
+#         citizen_count = familyMembers.objects.filter(familyHead_id = instance.familyHead).count()
+#         print(citizen_count)
+#         if citizen_count == instance.familyHead.totalFamilyMembers:
+#             instance.familyHead.partialSubmit = False
+#             instance.familyHead.save()
+
+
+# @receiver(post_save, sender=familyMembers)
+# def Update_Pending_Members_count(sender, instance, created , **kwargs):
+#     """
+#     The function creates a new user for a family head and assigns them to the "Family Head" group.
+    
+#     :param sender: The `sender` parameter refers to the object that is sending the signal. In this case,
+#     it could be any model instance that triggers the signal
+#     :param instance: The `instance` parameter is an instance of the model that triggered the signal. In
+#     this case, it is likely an instance of a model representing a family head
+#     """
+
+#     if created: 
+#         citizen_count = familyMembers.objects.filter(familyHead_id = instance.familyHead).count()
+        
+#         pending = instance.familyHead.totalFamilyMembers - citizen_count
+#         instance.familyHead.pendingMembers = pending
+#         instance.familyHead.save()
